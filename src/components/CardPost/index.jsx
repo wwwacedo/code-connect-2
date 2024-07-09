cf00aa0e24461737dc73dd43043f3d5724eba49f
wwwacedo <eng.marianammacedo@gmail.com>
2024-07-09 08:01:37 -0300

    bloco1 code connect pt3

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -4,29 +4,29 @@ import styles from './cardpost.module.css'
 import Link from "next/link"
 
 export const CardPost = ({ post, highlight }) => {
-    return (
-        <Link href={`/posts/${post.slug}`} className={styles.link}>
-             <article className={styles.card} style={{ width: highlight ? 993 : 486}}>
-                <header className={styles.header}>
-                    <figure style={{ height: highlight ? 300 : 133}}>
-                        <Image
-                            src={post.cover}
-                            fill
-                            alt={`Capa do post de titulo: ${post.title}`}
-                        />
-                    </figure>
-                </header>
-                <section className={styles.body}>
-                    <h2>{post.title}</h2>
-                    <p>{post.body}</p>
-                </section>
-                <footer className={styles.footer}>
-                    <Avatar
-                        imageSrc={post.author.avatar}
-                        name={post.author.username}
-                    />
-                </footer>
-            </article>
-        </Link>
-    )
+	return (
+		<article className={styles.card} style={{ width: highlight ? 993 : 486 }}>
+			<header className={styles.header}>
+				<figure style={{ height: highlight ? 300 : 133 }}>
+					<Image
+						src={post.cover}
+						fill
+						alt={`Capa do post de titulo: ${post.title}`}
+					/>
+				</figure>
+			</header>
+			<section className={styles.body}>
+				<h2>{post.title}</h2>
+				<p>{post.body}</p>
+				<Link href={`/posts/${post.slug}`}>Ver detalhes</Link>
+			</section>
+			<footer className={styles.footer}>
+				<Avatar
+					imageSrc={post.author.avatar}
+					name={post.author.username}
+				/>
+			</footer>
+		</article>
+
+	)
 }
